32e6415d0ecce046865758260d2767f63d4fdcee
Pablo Sanchez Narro <40486559@live.napier.ac.uk>
2022-03-16 19:17:59 +0000

    Report countries - largest population to smallest.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,11 +1,13 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
-/*
+/**
  *  Created by Group 7: Pablo Sanchez, Alex Vazquez, Sam Alman, Valentina Kerecanina
+ *  Main entry point of application
  *  Last Modification: 06/03/2022
- * */
+ */
 
 public class App {
     /**
@@ -18,10 +20,13 @@ public class App {
         // Connect to database
         a.connect();
 
-        // Get country
-        Country c = a.getCountry();
-        // Display results of the SQL query
-        a.displayCountry(c);
+        // Produce a report of all countries in the world organised by largest population to smallest
+        ArrayList<Country> allCountries = new ArrayList<>();
+        allCountries = a.getAllCountriesInWorld();
+
+        // Display results
+        System.out.println("Report on all the countries in the world organised by largest population to smallest.");
+        a.displayCountries(allCountries);
 
         // Disconnect from database
         a.disconnect();
@@ -84,45 +89,116 @@ public class App {
     }
 
     /**
-     * Get Country.
-     * @return Country object
+     * Returns all countries in the world, organised by the population from largest to smallest
+     * @return  list of Country objects
      * @author Pablo Sanchez
      */
-    public Country getCountry() {
+    public ArrayList<Country> getAllCountriesInWorld() {
+        ArrayList<Country> allCountries= new ArrayList<>(); // array to store all countries
+
         try {
+            // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT name "
-                            + "FROM country ";
+                    "SELECT Code, country.Name, Continent, Region, country.Population, city.Name AS 'Capital'"
+                            + "  FROM country LEFT JOIN city ON country.Capital = city.ID"
+                            + " ORDER BY Population DESC";
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            if (rset.next()) {
 
+            // while there are new rows in the result set, keep creating new country objects
+            while (rset.next())
+            {
                 Country c = new Country();
-                c.name = rset.getString("name");
-               // c.population = rset.getInt("population");
-
-                return c;
-            } else
-                return null;
-        } catch (Exception e) {
+                c.setCode(rset.getString("code"));
+                c.setName(rset.getString("name"));
+                c.setPopulation(Integer.parseInt(rset.getString("population")));
+                c.setContinent(rset.getString("continent"));
+                c.setRegion(rset.getString("region"));
+                c.setCapital(rset.getString("capital"));
+                allCountries.add(c);
+            }
+        }
+        catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get country details");
+            System.out.println("Failed to get country data");
+            return null;
+        }
+        return allCountries;
+    }
+
+    /**
+     * Displays a list of countries
+     * @param countryList
+     * @author Pablo Sanchez
+     */
+    public void displayCountries(ArrayList<Country> countryList) {
+        String h1 = "Code", h2 = "Name", h3 = "Continent", h4 = "Region", h5 = "Population", h6 = "Capital";
+        // print a header
+        System.out.println(String.format("%-4s %-44s %-14s %-25s %-10s %-34s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+        // print details of all countries in the list
+        for(Country c : countryList) {
+            String country = String.format("%-4s %-44s %-14s %-25s %-10d %-34s",
+                    c.getCode(), c.getName(), c.getContinent(), c.getRegion(), c.getPopulation(), c.getCapital());
+            System.out.println(country);
+        }
+    }
+
+    /**
+     * Create a Country object based on its code
+     * @param code  Country code
+     * @return      Country object created
+     * @author Pablo Sanchez
+     */
+    public Country getCountry(String code)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, population "
+                            + "FROM world.country "
+                            + "WHERE Code = " + code;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new country if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Country ctry = new Country();
+                ctry.setCode(rset.getString("code"));
+                ctry.setName(rset.getString("name"));
+                ctry.setPopulation(Integer.parseInt(rset.getString("population")));
+                return ctry;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country data");
             return null;
         }
     }
 
     /**
-     * Display information of the country object
+     * Display the country object's data
+     * @param c  Country object to display
      * @author Pablo Sanchez
      */
     public void displayCountry(Country c)
     {
         if (c != null)
         {
-            //Display countries from world
-            System.out.println(c.name + "\n");
+            System.out.println(
+                    c.getCode() + " "
+                            + c.getName() + " "
+                            + c.getPopulation() + "\n");
         }
     }
 }
--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -8,25 +8,164 @@ public class Country {
     /**
      * Code of the country
      */
-    public String code;
+    private String code;
 
     /**
      * Name of the country
      */
-    public String name;
-
-    /**
-     * Continent of the country
-     */
-    public String continent;
-
-    /**
-     * Region of the country
-     */
-    public String region;
+    private String name;
 
     /**
      * Population of the country
      */
-    public Integer population;
+    private int population;
+
+    /**
+     * Capital of the country
+     */
+    private String capital;
+
+    /**
+     * Continent of the country
+     */
+    private String continent;
+
+    /**
+     * Region of the country
+     */
+    private String region;
+
+    /**
+     * Default empty constructor
+     */
+    public Country() {
+    }
+
+    /**
+     * Creates a Country objects with parameters passed.
+     * @param code  Country's code
+     * @param name  Country name
+     * @param continent Country's continent
+     * @param region    Country's region
+     */
+    public Country(String code, String name, String continent, String region) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+    }
+
+    /**
+     * Creates a Country objects with parameters passed.
+     * @param code  Country's code
+     * @param name  Country name
+     * @param continent Country's continent
+     * @param region    Country's region
+     * @param population    Country's population
+     * @param capital   Country's capital code
+     */
+    public Country(String code, String name, String continent, String region, int population, String capital) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.population = population;
+        this.capital = capital;
+    }
+
+    /**
+     * Gets the code of the country
+     * @return  country code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Changes the value of the code of the country
+     * @param code  country code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * Gets the country name
+     * @return  country name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Changes the name of the country
+     * @param name  country name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the population of the country
+     * @return  country's population
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    /**
+     * Changes the population of the country
+     * @param population    population to set
+     */
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    /**
+     * Gets the capital of the country
+     * @return  country's capital
+     */
+    public String getCapital() {
+        return capital;
+    }
+
+    /**
+     * Changes the capital of the country
+     * @param capital   the country's capital to set
+     */
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    /**
+     * Gets the continent of the country
+     * @return  country's continent
+     */
+    public String getContinent() {
+        return continent;
+    }
+
+    /**
+     * Changes the continent of the country
+     * @param continent country's continent to set
+     */
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    /**
+     * Gets the region of the country
+     * @return  region of the country
+     */
+    public String getRegion() {
+        return region;
+    }
+
+    /**
+     * Changes the region of the country
+     * @param region region of the country to set
+     */
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
